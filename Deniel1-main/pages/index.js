0dd252d3d42f5d3eda015ae9bb364a2a1f46f928
unknown <blvdungo.student@ua.edu.ph>
2024-01-18 11:58:09 +0800

    first commit

--- Deniel1-main/pages/index.js
+++ Deniel1-main/pages/index.js
@@ -10,10 +10,27 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
 
-        <p>
-            1. 
-            {' '}
-        </p>
+        <p>My name is Deiniel R. David, I'am a IT student and currently
+            studying in the university of the assumption</p>
+            <p>
+              <ol>
+                <li>My expectation in this subject is always more knowledge to learn.</li>
+                <li>It's a good thing that Sir Robert is my prof in this subj because Sir Robert is a great guy</li>
+                <li>I expect to be able to utilize my skills in this course.</li>
+              </ol>
+            </p>
+            <p>
+<li>1. Being on time</li>
+<li>2. Making an effort</li>g
+<li>3. Being high energy</li>
+<li>4. Having a positive attitude</li>
+<li>5. Being passionate</li>
+<li>6. Using good body language</li>
+<li>7. Being coachable</li>
+<li>8. Doing a little extra</li>
+<li>9. Being prepared</li>
+<li>10. Having a strong work ethic</li>
+</p>
       </section>
     </Layout>
   );
